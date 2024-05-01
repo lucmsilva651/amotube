4ee0ba749fbb4d73f4023594c1fc00c3a10dd97b
Lucas Gabriel <90426410+lucmsilva651@users.noreply.github.com>
2024-05-01 16:11:32 -0300

    Base extension files

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# amotube
+# AmoTube (Amoled YouTube)
 Chrome extension that makes YouTube amoled.
+
+# Warning
+This is a beta version and sometimes it may not work.
+
+If you know anything to fix these issues, make a PR and I'll see if it is ok.
+
+# License
+2024 Lucas Gabriel (lucmsilva)
---
+++ content.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+  document.documentElement.style.setProperty('--ytd-searchbox-background', 'black');
+  document.documentElement.style.setProperty('--yt-spec-base-background', 'black');
+  document.documentElement.style.setProperty('--yt-spec-additive-background', '#0f0f0f');
+  document.documentElement.style.setProperty('--ytd-searchbox-legacy-button-color', 'black');
+
+}
