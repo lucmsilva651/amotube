6e8199088f62c3666e906168999cce4ab45665eb
Lucas Gabriel <90426410+lucmsilva651@users.noreply.github.com>
2024-05-03 20:47:12 -0300

    Update content.js

--- content.js
+++ content.js
@@ -1,7 +1,10 @@
 window.onload = function() {
   document.documentElement.style.setProperty('--ytd-searchbox-background', 'black');
+  document.documentElement.style.setProperty('--ytd-searchbox-legacy-button-color', 'black');
+  document.documentElement.style.setProperty('--ytd-searchbox-legacy-border-color', '0');
+  document.documentElement.style.setProperty('--yt-spec-outline', '#0f0f0f');
   document.documentElement.style.setProperty('--yt-spec-base-background', 'black');
   document.documentElement.style.setProperty('--yt-spec-additive-background', '#0f0f0f');
-  document.documentElement.style.setProperty('--ytd-searchbox-legacy-button-color', 'black');
-
+  document.documentElement.style.setProperty('--yt-spec-raised-background', '#0f0f0f');
+  document.documentElement.style.setProperty('--yt-spec-menu-background', "#0f0f0f");
 }
