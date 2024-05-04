671734496d7ce85816a10b536ef30c762d702f41
GiovaniFZ <giovanifinazzi@gmail.com>
2024-05-04 19:50:04 -0300

    Apply themes correctly Our Amoled theme should be applied only if: - The user sets YouTube to dark - Or the system theme is dark Before, if the user had a light theme, youtube would be reloading forever, this patch corrects it.

--- js/amotube.js
+++ js/amotube.js
@@ -1,4 +1,16 @@
-window.onload = function() {
+// Getting current primary_color value
+var style = getComputedStyle(document.documentElement);
+var prim_color = style.getPropertyValue('--yt-spec-brand-background-primary');
+
+// We are going to set the theme if color is not white.
+// if prim_color is #fff, it means it's on dark mode
+if(prim_color == "#fff"){
+  console.log("It's light so theme won't be applied")
+}else{
+  setTheme();
+}
+
+function setTheme(){
   document.documentElement.style.setProperty('--ytd-searchbox-background', 'black');
   document.documentElement.style.setProperty('--ytd-searchbox-legacy-button-color', 'black');
   document.documentElement.style.setProperty('--ytd-searchbox-legacy-border-color', '#0f0f0f');
@@ -9,4 +21,3 @@ window.onload = function() {
   document.documentElement.style.setProperty('--yt-spec-raised-background', '#0f0f0f');
   document.documentElement.style.setProperty('--yt-spec-menu-background', "#0f0f0f");
 }
-
