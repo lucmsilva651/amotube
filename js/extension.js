e53d48d90fae860a7ecf3321acf5f316eb3cb71c
Luquinhas <lucmsilva651@gmail.com>
2025-06-26 12:20:06 -0300

    Fix logic + improve handling

--- js/extension.js
+++ js/extension.js
@@ -1,24 +1,42 @@
-const modifyElement = (e, f) => document.documentElement.style.setProperty(e, f);
+const modifyElement = (e, f, p) => document.documentElement.style.setProperty(e, f, p);
 
 const style = getComputedStyle(document.documentElement);
-const ytBgColor = style.getPropertyValue('--yt-spec-base-background');
+const ytBgColor = style.getPropertyValue("--yt-spec-base-background");
 
-function setTheme() {
+async function resetImportant() {
+  modifyElement("--ytd-searchbox-background", "#000000", null);
+  modifyElement("--ytd-searchbox-legacy-button-color", "#000000", null);
+  modifyElement("--ytd-searchbox-legacy-border-color", "#0f0f0f", null);
+  modifyElement("--ytd-searchbox-legacy-button-border-color", "#0f0f0f", null);
+  modifyElement("--yt-spec-outline", "#0f0f0f", null);
+  modifyElement("--yt-spec-base-background", "#000000", null);
+  modifyElement("--yt-spec-additive-background", "#0f0f0f", null);
+  modifyElement("--yt-spec-raised-background", "#0f0f0f", null);
+  modifyElement("--yt-spec-menu-background", "#0f0f0f", null);
+}
+
+async function setTheme() {
   if (ytBgColor == "#fff") {
     console.log("Light theme active")
     return;
   } else {
     console.log("Dark theme active");
-    modifyElement('--ytd-searchbox-background', 'black');
-    modifyElement('--ytd-searchbox-legacy-button-color', 'black');
-    modifyElement('--ytd-searchbox-legacy-border-color', '#0f0f0f');
-    modifyElement('--ytd-searchbox-legacy-button-border-color', '#0f0f0f');
-    modifyElement('--yt-spec-outline', '#0f0f0f');
-    modifyElement('--yt-spec-base-background', 'black');
-    modifyElement('--yt-spec-additive-background', '#0f0f0f');
-    modifyElement('--yt-spec-raised-background', '#0f0f0f');
-    modifyElement('--yt-spec-menu-background', "#0f0f0f");
+    modifyElement("--ytd-searchbox-background", "#000000", "important");
+    modifyElement("--ytd-searchbox-legacy-button-color", "#000000", "important");
+    modifyElement("--ytd-searchbox-legacy-border-color", "#0f0f0f", "important");
+    modifyElement("--ytd-searchbox-legacy-button-border-color", "#0f0f0f", "important");
+    modifyElement("--yt-spec-outline", "#0f0f0f", "important");
+    modifyElement("--yt-spec-base-background", "#000000", "important");
+    modifyElement("--yt-spec-additive-background", "#0f0f0f", "important");
+    modifyElement("--yt-spec-raised-background", "#0f0f0f", "important");
+    modifyElement("--yt-spec-menu-background", "#0f0f0f", "important");
   }
 }
 
-setTheme();
+async function doWork() {
+  console.log("AmoTube is running");
+  await resetImportant()
+  setTheme();
+}
+
+doWork();
